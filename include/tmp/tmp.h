091e9e34b2cbcdfac7f2f0a36f5828426a05f679
Filip Dutescu <filip.dutescu@gmail.com>
2020-05-17 23:29:11 +0300

    added function in *.h and *.cpp, added proper structure, more standard settings

---
+++ include/tmp/tmp.h
@@ -0,0 +1,9 @@
+#ifndef TMP_TMP_H_
+#define TMP_TMP_H_
+
+namespace tmp
+{
+    int add(int, int);
+};
+
+#endif // TMP_TMP_H_
---
+++ src/tmp.cpp
@@ -0,0 +1,6 @@
+#include "tmp/tmp.h"
+
+int tmp::add(int a, int b)
+{
+    return a + b;
+}
