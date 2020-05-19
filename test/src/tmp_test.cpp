0e448041923231785b77ebe968a47c73a7a5640d
Filip Dutescu <filip.dutescu@gmail.com>
2020-05-19 19:25:30 +0300

    working on GoogleTest integration

--- test/src/tmp_test.cpp
+++ test/src/tmp_test.cpp
@@ -2,6 +2,14 @@
 
 #include "tmp/tmp.h"
 
-TEST(TmpAddTest, CheckValues) {
-    EXPECT_EQ(tmp::add(1, 2), 3)
+TEST(TmpAddTest, CheckValues)
+{
+    ASSERT_EQ(tmp::add(1, 2), 3);
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
