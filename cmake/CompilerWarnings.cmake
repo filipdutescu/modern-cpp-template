3e510e0697c546e7e3d836bff882571e7e252add
Filip Dutescu <filip.dutescu@gmail.com>
2020-05-30 21:32:13 +0300

    added support for: Clang-Tidy, Cppcheck and Catch2

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -5,10 +5,8 @@
 # Courtesy of Jason Turner
 
 function(set_project_warnings project_name)
-  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
-
   set(MSVC_WARNINGS
-      /W4 # Baseline reasonable warnings
+      /W4     # Baseline reasonable warnings
       /w14242 # 'identifier': conversion from 'type1' to 'type1', possible loss
               # of data
       /w14254 # 'operator': conversion from 'type1:field_bits' to
@@ -43,20 +41,20 @@ function(set_project_warnings project_name)
 
   set(CLANG_WARNINGS
       -Wall
-      -Wextra # reasonable and standard
+      -Wextra  # reasonable and standard
       -Wshadow # warn the user if a variable declaration shadows one from a
                # parent context
       -Wnon-virtual-dtor # warn the user if a class with virtual functions has a
                          # non-virtual destructor. This helps catch hard to
                          # track down memory errors
       -Wold-style-cast # warn for c-style casts
-      -Wcast-align # warn for potential performance problem casts
-      -Wunused # warn on anything being unused
+      -Wcast-align     # warn for potential performance problem casts
+      -Wunused         # warn on anything being unused
       -Woverloaded-virtual # warn if you overload (not override) a virtual
                            # function
-      -Wpedantic # warn if non-standard C++ is used
+      -Wpedantic   # warn if non-standard C++ is used
       -Wconversion # warn on type conversions that may lose data
-      -Wsign-conversion # warn on sign conversions
+      -Wsign-conversion  # warn on sign conversions
       -Wnull-dereference # warn if a null dereference is detected
       -Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output
@@ -89,6 +87,9 @@ function(set_project_warnings project_name)
     message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
   endif()
 
-  target_compile_options(${project_name} INTERFACE ${PROJECT_WARNINGS})
+  target_compile_options(${project_name} PUBLIC ${PROJECT_WARNINGS})
 
+  if(NOT TARGET ${project_name})
+    message(AUTHOR_WARNING "${project_name} is not a target, thus no compiler warning were added.")
+  endif()
 endfunction()
--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -1,12 +1,38 @@
 #
 # Generate compile_commands.json for clang based tools
 #
+
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+#
+# Compiler options
+#
+
+option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
+
+#
+# Static analyzers
+#
+# Currently supporting: Clang-Tidy, Cppcheck.
+
+option(ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." ON)
+option(ENABLE_CPPCHECK "Enable static analysis with Cppcheck." OFF)
+
+#
+# Unit testing
+#
+# Currently supporting: GoogleTest.
+
+option(ENABLE_UNIT_TESTING "Enable unit tests for the projects (from the `test` subfolder)." ON)
+option(USE_GTEST "Use the GoogleTest project for creating unit tests." ON)
+option(USE_CATCH2 "Use the Catch2 project for creating unit tests." ON)
+
+#
+# Miscelanious options
+#
 
 option(VERBOSE_OUTPUT "Enable verbose output, allowing for a better understanding of each step taken." ON)
 option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)." OFF)
-option(ENABLE_UNIT_TESTING "Enable unit tests for the projects (from the `test` subfolder)." ON)
-option(USE_GTEST "Use the GoogleTest project for creating unit tests." ON)
 
 if(ENABLE_IPO)
     include(CheckIPOSupported)
