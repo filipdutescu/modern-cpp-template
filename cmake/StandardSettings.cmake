f5d00075f25c97600305c57e1ad3f645b29e2c48
Filip Dutescu <filip.dutescu@gmail.com>
2020-06-02 11:26:11 +0300

    added: GoogleMock support

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -20,6 +20,8 @@ option(ENABLE_VCPKG "Enable the Vcpkg package manager for this project." OFF)
 option(ENABLE_UNIT_TESTING "Enable unit tests for the projects (from the `test` subfolder)." ON)
 
 option(USE_GTEST "Use the GoogleTest project for creating unit tests." ON)
+option(USE_GOOGLE_MOCK "Use the GoogleMock project for extending the unit tests." OFF)
+
 option(USE_CATCH2 "Use the Catch2 project for creating unit tests." ON)
 
 #
