197ca81a8332cc0e28160f61833c6deff690f678
Filip Dutescu <filipdutescu>
2020-06-16 12:01:45 +0300

    corrected output dir for doxygen [skip ci]

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -2,17 +2,10 @@ if(${PROJECT_NAME}_ENABLE_DOXYGEN)
     set(DOXYGEN_CALLER_GRAPH YES)
     set(DOXYGEN_CALL_GRAPH YES)
     set(DOXYGEN_EXTRACT_ALL YES)
+    set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/docs)
+
     find_package(Doxygen REQUIRED dot)
     doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
-
-    add_custom_target(
-        generate-docs
-	COMMAND
-            ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.doxygen-docs
-        WORKING_DIRECTORY
-            ${CMAKE_CURRENT_SOURCE_DIR}/docs
-	VERBATIM
-    )
 
     verbose_message("Doxygen has been setup and documentation is now available.")
 endif()
