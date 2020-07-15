051b505c638ced08d9ed41e6e0d1fccee6f0ffe2
Filip Dutescu <filip.dutescu@gmail.com>
2020-07-15 23:14:12 +0300

    changed Catch2 default enabling [skip ci]

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -30,7 +30,7 @@ option(${PROJECT_NAME}_ENABLE_UNIT_TESTING "Enable unit tests for the projects (
 option(${PROJECT_NAME}_USE_GTEST "Use the GoogleTest project for creating unit tests." ON)
 option(${PROJECT_NAME}_USE_GOOGLE_MOCK "Use the GoogleMock project for extending the unit tests." OFF)
 
-option(${PROJECT_NAME}_USE_CATCH2 "Use the Catch2 project for creating unit tests." ON)
+option(${PROJECT_NAME}_USE_CATCH2 "Use the Catch2 project for creating unit tests." OFF)
 
 #
 # Static analyzers
