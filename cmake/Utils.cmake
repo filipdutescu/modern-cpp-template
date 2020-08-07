68ed3319d17cc3a87d37700b1e20422023202c9f
Adem Budak <admbudk@gmail.com>
2020-08-07 16:13:37 +0300

    Remove redundant newline ('\n') characters on end the message.
    The message() command of the CMake inserts newline on the end of its
    argument.

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -20,6 +20,6 @@ function(add_clang_format_target)
     add_custom_target(clang-format
         COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
         -i ${CMAKE_CURRENT_LIST_DIR}/${sources} ${CMAKE_CURRENT_LIST_DIR}/${headers})
-    message("Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).\n")
+    message("Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).")
     endif()
 endfunction()
