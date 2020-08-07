f64f3d3ec76078f8200ac5c794c5e80661a71da3
Adem Budak <admbudk@gmail.com>
2020-08-07 16:19:22 +0300

    Provide some more hints on linting with clang-tidy.
    Lists the clang-tidy options for linting and use the compilation
    database to which files to lint.

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,7 +1,36 @@
 if(${PROJECT_NAME}_ENABLE_CLANG_TIDY)
   find_program(CLANGTIDY clang-tidy)
+
+#  Enable checks by removing '-' in front of them:
+#  for example enable performance checks change:
+# '-performance-*' to 'performance-*'
   if(CLANGTIDY)
-    set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
+    set(CMAKE_CXX_CLANG_TIDY
+        clang-tidy
+        -p=${CMAKE_BINARY_DIR}/compile_commands.json
+        -abseil-*,
+        -android-*,
+        -boost-*,
+        -bugprone-*,
+        -cert-*,
+        -clang-*,
+        cppcoreguidelines-*,
+        -darwin-*,
+        -fuchsia-*,
+        -google-*,
+        -hicpp-*,
+        -linuxkernel-*,
+        -llvm-*,
+        -llvmlibc-*,
+        -misc-*,
+        -modernize-*,
+        -mpi-*,
+        -objc-*,
+        -openmp-*,
+        -performance-*,
+        -portability-*,
+        -readability-*,
+        -zircon-*)
     message("Clang-Tidy finished setting up.")
   else()
     message(SEND_ERROR "Clang-Tidy requested but executable not found.")
@@ -11,8 +40,14 @@ endif()
 if(${PROJECT_NAME}_ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
   if(CPPCHECK)
-    set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --suppress=missingInclude --enable=all
-                           --inline-suppr --inconclusive -i ${CMAKE_SOURCE_DIR}/imgui/lib)
+    set(CMAKE_CXX_CPPCHECK
+        ${CPPCHECK}
+        --suppress=missingInclude
+        --enable=all
+        --inline-suppr
+        --inconclusive
+        -i
+        ${CMAKE_SOURCE_DIR}/imgui/lib)
     message("Cppcheck finished setting up.")
   else()
     message(SEND_ERROR "Cppcheck requested but executable not found.")
