d302ba5115bc3267455557f9bf7071b01deabeb6
Filip Dutescu <filip.dutescu@gmail.com>
2020-08-15 12:41:38 +0300

    fixed 'clang-format' target for different build types [skip ci]

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -14,12 +14,24 @@ endfunction()
 
 function(add_clang_format_target)
     if(NOT ${PROJECT_NAME}_CLANG_FORMAT_BINARY)
-    find_program(${PROJECT_NAME}_CLANG_FORMAT_BINARY clang-format)
+			find_program(${PROJECT_NAME}_CLANG_FORMAT_BINARY clang-format)
     endif()
+
     if(${PROJECT_NAME}_CLANG_FORMAT_BINARY)
-    add_custom_target(clang-format
-        COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-        -i ${CMAKE_CURRENT_LIST_DIR}/${sources} ${CMAKE_CURRENT_LIST_DIR}/${headers})
-		message(STATUS "Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).\n")
+			if(${PROJECT_NAME}_BUILD_EXECUTABLE)
+				add_custom_target(clang-format
+						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
+						-i $${CMAKE_CURRENT_LIST_DIR}/${exe_sources} ${CMAKE_CURRENT_LIST_DIR}/${headers})
+			elseif(${PROJECT_NAME}_BUILD_HEADERS_ONLY)
+				add_custom_target(clang-format
+						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
+						-i ${CMAKE_CURRENT_LIST_DIR}/${headers})
+			else()
+				add_custom_target(clang-format
+						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
+						-i ${CMAKE_CURRENT_LIST_DIR}/${sources} ${CMAKE_CURRENT_LIST_DIR}/${headers})
+			endif()
+
+			message(STATUS "Format the project using the `clang-format` target (i.e: cmake --build build --target clang-format).\n")
     endif()
 endfunction()
