0d8a0acdcb383d6112fa19e88302c96fe5327eef
Filip Dutescu <filip.dutescu@gmail.com>
2021-08-06 00:04:14 +0300

    fix CMakeLists.txt

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -37,7 +37,7 @@ option(${PROJECT_NAME}_USE_CATCH2 "Use the Catch2 project for creating unit test
 #
 # Currently supporting: Clang-Tidy, Cppcheck.
 
-option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." ON)
+option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
 option(${PROJECT_NAME}_ENABLE_CPPCHECK "Enable static analysis with Cppcheck." OFF)
 
 #
